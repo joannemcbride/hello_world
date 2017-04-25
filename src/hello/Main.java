6cb59bce8cce2e0204fd5829962f63a7b02850b0
joannemcbride <joanne.g.mcbride@gmail.com>
2017-04-24 20:54:42 -0700

    Added date display

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -1,8 +1,11 @@
 package hello;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello brave new world - from sayHello branch");
+        System.out.println(new Date());
     }
 }
