b4325d544fa367f7e1c216290e2a198b825361dc
SYTC311u06 <SYTC311u06@ad.pcc.edu>
2017-04-24 20:11:37 -0700

    Modified println

--- src/hello/Main.java
+++ src/hello/Main.java
@@ -3,6 +3,6 @@ package hello;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello cs233J world");
+        System.out.println("Hello brave new world");
     }
 }
